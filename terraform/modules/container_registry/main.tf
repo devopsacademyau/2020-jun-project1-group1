f8039bf77d0d5442bb5b2f4eb144d2d3f9fc5fd4
Alan Lima <alima@outlook.com>
2020-07-24 08:44:32 +1000

    WIP: ecr terraform code
    add terraform code and example to create the aws ecr repository
    
    add instance access policy to authenticate and pull images from ecr registry
    
    finish work in the ECR terraform module

---
+++ terraform/modules/container_registry/main.tf
@@ -0,0 +1,11 @@
+resource "aws_ecr_repository" "repo" {
+  name                 = var.repository_name
+  image_tag_mutability = "MUTABLE"
+  tags = merge(var.common_tags, {
+    Name = "devops-${var.repository_name}"
+  })
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
