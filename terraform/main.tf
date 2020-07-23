06009febe73554180ea6223ccb4b2eac77cbb17c
Mariana Freitas <mfreitas.sm@gmail.com>
2020-07-23 22:27:27 +1000

    Add ssm parameter inside rds module

---
+++ terraform/main.tf
@@ -0,0 +1,3 @@
+module "rds" {
+  source = "./modules/rds"
+}
---
+++ terraform/modules/rds/main.tf
@@ -0,0 +1,32 @@
+
+resource "aws_ssm_parameter" "db_host" {
+  name        = "WORDPRESS_DB_HOST"
+  description = "Database Host Paramater"
+  type        = "SecureString"
+  value       = ""
+  overwrite   = true
+}
+
+resource "aws_ssm_parameter" "db_name" {
+  name        = "WORDPRESS_DB_NAME"
+  description = "Database Name Paramater"
+  type        = "SecureString"
+  value       = ""
+  overwrite   = true
+}
+
+resource "aws_ssm_parameter" "db_user" {
+  name        = "WORDPRESS_DB_USER"
+  description = "Database User Paramater"
+  type        = "SecureString"
+  value       = ""
+  overwrite   = true
+}
+
+resource "aws_ssm_parameter" "db_password" {
+  name        = "WORDPRESS_DB_PASSWORD"
+  description = "Database Password Paramater"
+  type        = "SecureString"
+  value       = ""
+  overwrite   = true
+}
