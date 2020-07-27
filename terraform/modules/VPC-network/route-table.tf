bc8ab5713315656c91ac212cc9995a3cb6f96f99
ZaraJoy <64997373+zarajoy@users.noreply.github.com>
2020-07-28 09:17:18 +1000

    first cut of vpn network (#18)
    * first cut of vpn network - updated as per comments
    
    * remove main and variables from modules folder

---
+++ terraform/modules/VPC-network/route-table.tf
@@ -0,0 +1,48 @@
+# Route tables and assosications
+# public
+resource "aws_route_table" "route-table-PUB" {
+    vpc_id = aws_vpc.vpc.id
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.IGW.id
+    }
+    tags = {
+	Name = "${var.project-name}-route-table-PUB"
+	}	
+}
+# private
+resource "aws_route_table" "route-table-PRI" {
+    vpc_id = aws_vpc.vpc.id
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_nat_gateway.NAT-GW.id
+    }
+    tags = {
+	Name = "${var.project-name}-route-table-PRI"
+	}
+		
+}
+
+# route table associations
+# public route tables
+resource "aws_route_table_association" "public-1" {
+    subnet_id = aws_subnet.subnet-public-1.id
+    route_table_id = aws_route_table.route-table-PUB.id
+}
+
+resource "aws_route_table_association" "public-2" {
+    subnet_id = aws_subnet.subnet-public-2.id
+    route_table_id = aws_route_table.route-table-PUB.id
+}
+
+# private route tables
+resource "aws_route_table_association" "private-1" {
+    subnet_id = aws_subnet.subnet-private-1.id
+    route_table_id = aws_route_table.route-table-PRI.id
+}
+
+resource "aws_route_table_association" "private-2" {
+    subnet_id = aws_subnet.subnet-private-1.id
+    route_table_id = aws_route_table.route-table-PRI.id
+}
+
---
+++ terraform/modules/VPC-network/variables.tf
@@ -0,0 +1,36 @@
+#variable declaration
+variable "project-name" {
+  type = string
+}
+
+variable "vpcCIDR" {
+    description = "VPC network"
+    default     = "192.168.0.0/16"
+}
+
+variable "instanceTenancy" {
+    type    = list(string)
+    description = "defult or dedicated"
+    default = ["default","dedicated"]
+}
+
+variable "dnsSupport" {
+    type    = bool
+    default = true
+    
+}
+
+variable "dnsHostNames" {
+    type    = bool
+    default = true
+}
+
+variable "map_public_ip1" {
+    type    = bool
+    default = true
+}
+
+variable "map_public_ip2" {
+    type    = bool
+    default = true
+}
