7c00faa16599a0a32bf68014aaa5e4c19c3b4fd5
Alan Lima <alima@outlook.com>
2020-08-17 01:54:38 +1000

    docs update and use local state as default

--- terraform/_backend.tf
+++ terraform/_backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "da-devops-terraform"
-    key    = "terraform.tfstate"
-    region = "ap-southeast-2"
-    dynamodb_table = "da-devops-terraform-lock"
+  backend "local" {
+    
   }
 }
