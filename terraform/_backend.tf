9c0ba4680f227b48cdce01e7dda03918882aada6
Alan Lima <alanlima@users.noreply.github.com>
2020-08-17 00:43:31 +1000

    update backend file (#50)

--- terraform/_backend.tf
+++ terraform/_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "da-devops-terraform-state"
+    bucket = "da-devops-terraform"
     key    = "terraform.tfstate"
     region = "ap-southeast-2"
     dynamodb_table = "da-devops-terraform-lock"
